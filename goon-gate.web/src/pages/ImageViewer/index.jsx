f6bf814c819f7528212d8b53e4300cc2e01bb189
Stefan Aleksic <stefan@stefanaleksic.com>
2017-11-05 01:52:35 -0700

    Add QR code and Image Viewer

--- goon-gate.web/src/pages/ImageViewer/index.jsx
+++ goon-gate.web/src/pages/ImageViewer/index.jsx
@@ -6,7 +6,14 @@ import IPFSImageWrapper from 'sections/IPFSImageWrapper';
 
 import './index.css';
 
+const {location} = window;
+
 export default class ImageViewer extends Component {
+  getHashFromURL() {
+    const urlParts = location.href.split("/") ;
+    return urlParts[urlParts.length - 1];
+  }
+
   render() {
     return (
       <div className="ImageViewer">
